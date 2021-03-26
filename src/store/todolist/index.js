93011ce327e58aaa288b561704ac31c3945bd4c6
shamssyho <shamss12301230@gmail.com>
2021-03-26 02:22:25 +0100

    Add files via upload

---
+++ src/store/todolist/index.js
@@ -0,0 +1,27 @@
+import state from './state'
+import * as getters from './getters'
+import * as mutations from './mutations'
+import * as actions from './actions'
+export default {
+  namespaced: true,
+  getters,
+  mutations,
+  actions,
+  state
+}
+
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT// COMMENT
+// COMMENT
+// COMMENT
+// COMMENT

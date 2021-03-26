290f30fa5c4d35bdad1e48cd6812bb8e86c12b38
shamssyho <shamss12301230@gmail.com>
2021-03-26 02:29:22 +0100

    Add files via upload

---
+++ src/views/about.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        
+        <h1>Info sur le projet</h1>
+        <h2>Les membres de groupes</h2>
+            <ul>
+            <li>Yahya HASNAOUI 21912359</li>
+            <li>Mohamad ALSAADI 21811312</li>
+            </ul>
+        <p> notre objet est de créer un todo list à l'aide de Vue.js praticulièrement </p>
+        <p>Taches faites : architecture de sossier 
+            ajouter des taches 
+            les supprimer 
+            Initialisation d'une tâche faite
+            Barrer les taches faites
+            Supprimer les taches même s'elles ne sont pas faites
+        </p>
+
+            <h2>Les sources utilisé :</h2>
+            <ul>
+                <li>Cours et tp </li>
+                <li>w3school</li>
+                <li>https://router.vuejs.org/guide/essentials/navigation.html</li>
+                <li>https://www.youtube.com/watch?v=SgtS7jWF2O4&list=PLjwdMgw5TTLW-mAtlR46VajrKs4dep3y0&index=11</li>
+                <li>https://www.youtube.com/watch?v=5sNXjRE1C-U</li>
+                <li>https://stackoverflow.com/questions/35459010/vuejs-remove-element-from-lists</li>
+                <li>https://getbootstrap.com/</li>
+            </ul>
+    </section>
+</template>
+      <script>
+      export default {
+          name: 'about',
+      
+      }
+      </script>
+    

3aeccb9e39d121eabe1727317631c7bef1d84d07
shamssyho <shamss12301230@gmail.com>
2021-03-26 02:20:51 +0100

    Add files via upload

---
+++ src/store/todolist/action.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export function load({ commit }){
+    axios.get('json/immo.json')
+            .then(response => {
+                commit("load", response.data);
+            })
+
+}
+// comment
